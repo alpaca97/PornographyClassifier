17399b9bce4b5efd44b90fe78cd9b551f84923fb
Alex Pachter <alexpachter@800G2-1767.DisStu.local>
2018-05-02 11:14:05 +0200

    updateindex

--- localhost/main.js
+++ localhost/main.js
@@ -1,11 +1,13 @@
-var main = function(classifier) { # 
+var main = function(classifier) { 
 
-title = document.getElementById("mySubmit");
+
+
+title = document.getElementById("title").value;
 // https://developer.mozilla.org/en-US/docs/Web/API/FormData/get
 };
 
 
 
 $(document).ready(function(){
-	Promise.all(['ClassifierJSONfile', ].map($.get)).then(main); 
+	Promise.all(['model.json','model.h5','porn_classifier.py'].map($.get)).then(main); 
 })
